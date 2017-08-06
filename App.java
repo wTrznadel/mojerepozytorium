f7381e19ca6add451f52da8fcf9c7b24badfe219
wTrznadel <30767303+wTrznadel@users.noreply.github.com>
2017-08-06 11:47:51 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,19 @@
+package NaukaJavy.Projekt1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Kot asta = new Kot ();
+        asta.imie = "Asta";
+        asta.imieOpiekuna = "Wiktoria";
+        asta.waga = 3.3f;
+        asta.przedstawSie();
+       // asta.dataUrodzenia = "2000-01-01";
+    }
+}
